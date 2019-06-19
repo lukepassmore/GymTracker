6c3b89c36ec1a2e505c176705787db2cad43ebe3
Luke Passmore <mail@lukepassmore.co.uk>
2019-06-19 14:52:59 +0100

    Added Native Routing

--- App.js
+++ App.js
@@ -1,49 +1,121 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {Component} from "react";
+import {Platform, StyleSheet, Text, View, AppRegistry} from "react-native";
+import { NativeRouter, Route, Link } from "react-router-native";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+const Home = () => <Text style={styles.header}>Home</Text>;
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const About = () => <Text style={styles.header}>About</Text>;
+
+const Topic = ({ match }) => (
+  <Text style={styles.topic}>{match.params.topicId}</Text>
+);
+
+const Topics = ({ match }) => (
+  <View>
+    <Text style={styles.header}>Topics</Text>
+    <View>
+      <Link
+        to={`${match.url}/rendering`}
+        style={styles.subNavItem}
+        underlayColor="#f0f4f7"
+      >
+        <Text>Rendering with React</Text>
+      </Link>
+      <Link
+        to={`${match.url}/components`}
+        style={styles.subNavItem}
+        underlayColor="#f0f4f7"
+      >
+        <Text>Components</Text>
+      </Link>
+      <Link
+        to={`${match.url}/props-v-state`}
+        style={styles.subNavItem}
+        underlayColor="#f0f4f7"
+      >
+        <Text>Props v. State</Text>
+      </Link>
+    </View>
+
+    <Route path={`${match.path}/:topicId`} component={Topic} />
+    <Route
+      exact
+      path={match.path}
+      render={() => <Text style={styles.topic}>Please select a topic.</Text>}
+    />
+  </View>
+);
+
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+<NativeRouter>
+
+    <View style={styles.container}>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </View>
+
+    <View style={styles.nav}>
+        <Link to="/about" underlayColor="#f0f4f7" style={styles.navItem}>
+          <Text>About</Text>
+        </Link>
+        <Link to="/" underlayColor="#f0f4f7" style={styles.navItem}>
+          <Text>Home</Text>
+        </Link>
+        <Link to="/topics" underlayColor="#f0f4f7" style={styles.navItem}>
+          <Text>Topics</Text>
+        </Link>
       </View>
+
+  </NativeRouter>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 25,
+    padding: 10,
+    flex: 1
+  },
+  header: {
+    fontSize: 20
+  },
+  nav: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    height: 50
+  },
+  navItem: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: "center",
+    padding: 10
+  },
+  subNavItem: {
+    padding: 5
+  },
+  topic: {
+    textAlign: "center",
+    fontSize: 15
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
+  header: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  main: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
 });
+AppRegistry.registerComponent("GymTracker", () => App);
